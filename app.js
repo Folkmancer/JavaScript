6b65f3d9f93ac7ec5872c402fe7485bb4c74e134
Folkmancer <folkmancer@gmail.com>
2019-12-01 13:37:04 +0300

    finished the chapter Objects

--- app.js
+++ app.js
@@ -15,8 +15,46 @@ schedule["8:30"] = "get up";
 alert(isEmpty(schedule));
 
 function isEmpty(obj) {
-    for (let key in obj){
+    for (let key in obj) {
         return false;
     }
     return true; 
+}
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+let sum = 0;
+
+for (let key in salaries) {
+    sum += salaries[key];
+}
+    
+alert(sum);
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+for (let key in menu) {
+    alert(menu[key]);
+}
+
+multiplyNumeric(menu);
+
+for (let key in menu) {
+    alert(menu[key]);
+}
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
+    }
 }
