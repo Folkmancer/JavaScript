e2ee91ddcae1965909fb03fb51fcde5607cf6194
Folkmancer <folkmancer@gmail.com>
2019-12-10 23:05:02 +0300

    finished the chapter Object methods, "this"

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ let sum = 0;
 for (let key in salaries) {
     sum += salaries[key];
 }
-    
+
 alert(sum);
 
 let menu = {
@@ -43,3 +43,45 @@ multiplyNumeric(menu);
 for (let key in menu) {
     alert(menu[key]);
 }
+
+let calculator = {
+    x: 0,
+    y: 0,
+    read() {
+        this.x = +prompt("Введите первое число:", 0);
+        this.y = +prompt("Введите второе число:", 0);
+    },
+    sum() {
+        return this.x + this.y;
+    },
+    mul() {
+        return this.x * this.y;
+    }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep() {
+        alert(this.step);
+        return this;
+    }
+}
+
+ladder.up()
+    .up()
+    .down()
+    .up()
+    .down()
+    .showStep();
